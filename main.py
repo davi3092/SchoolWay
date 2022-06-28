1916bb3d3b1c5bb78d61472a70e28e30e527c673
Gustavo Rodrigues Lima <83222330+GustavoRodrigueess@users.noreply.github.com>
2022-06-28 16:23:39 -0300

    Arquivo Principal

---
+++ main.py
@@ -0,0 +1,35 @@
+import requests
+import sys
+import json
+import time
+
+bingMapsKey = ""
+
+def name():
+    nome = input("Digite seu nome: ")
+    return nome
+
+def quantidade_de_alunos():
+    numero_de_alunos = int(input("Digite a quantidade de alunos que você tem: "))
+    return numero_de_alunos
+
+def ponto_inicial():
+    origem = input("Digite o ponto inicial: ")
+    return origem
+
+origin = ponto_inicial()
+
+def ponto_final():
+    destinatario = input("Digite o ponto final: ")
+    return destinatario
+
+destination = ponto_final()
+
+if not bingMapsKey or bingMapsKey == "":
+    print("Token não foi inserido corretamente!")
+    sys.exit()
+
+route = "http://dev.virutalearth.net/REST/V1/Routes/Driving?wp.0=" + origin + "&wp.1=" + destination + "/&key=" + bingMapsKey
+print("Printando rota... \n")
+time.sleep(3)
+print(route)
